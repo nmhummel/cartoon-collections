e09da5976a11a2cf8e54ca896d396ed16e48a981
nmhummel <natalie.hummel@gmail.com>
2020-10-23 22:13:20 -0500

    commit tree 8b1c404b88883a53c79df4b90cb462b55a7b7815

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+# Get familiar iterating through arrays with enumerator methods like .collect or .map, .find, and .include?.
+# Build methods and control their return values.
+# Practice control flow with if and else statements.
+
+
+def roll_call_dwarves(dwarves) # This method should accept an array of dwarf names
+  dwarf_list = []
+  dwarves.each.with_index(1) do |dwarf, index|
+    dwarf_list.push("> #{index}. #{dwarf}")
+  end
+    puts dwarf_list.join(" ")
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|element| element.capitalize + "!"}
+end
+ 
+def long_planeteer_calls(calls)
+  calls.any? {|call| call.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(hodgepodge)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  hodgepodge.detect {|yo_cheese| cheese_types.include?(yo_cheese)}
 end
